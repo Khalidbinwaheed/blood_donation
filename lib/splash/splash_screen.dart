790541360a9466f5fd8552ab8d653894ddd7ceb5
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-24 23:12:54 +0500

    Create splash_screen.dart

---
+++ lib/splash/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
