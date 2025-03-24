de76423e3fb83e524f50dddc3fa445db93ea045e
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-24 23:58:30 +0500

    Update routes.dart

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,10 @@
+import 'package:blood_donation/feathers/user_managment/presentation/screens/main_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../splash/splash_screen.dart';
+part 'routes.g.dart';
 
 enum AppRoutes {
   splash,
@@ -11,14 +17,24 @@ enum AppRoutes {
   notifications,
 }
 
+@riverpod
+
 GoRouter goRouter(GoRouterRef ref) {
   return GoRouter(
+    initialLocation: '/splash',
+    debugLogDiagnostics: true,
     routes: [
       GoRoute(
         path: '/splash',
         name: AppRoutes.splash.name,
         builder: (ctx, state) => const SplashScreen(),
       ),
+      GoRoute(
+        path: '/main',
+        name: AppRoutes.splash.name,
+        builder: (ctx, state) => const MainScreen(),
+      ),
+     
     ],
   );
 }
