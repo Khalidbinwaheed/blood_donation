a4b5a89f77018bde4ec50ba8457a21f4466cc2b1
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-24 23:23:30 +0500

    Update splash_screen.dart

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -6,20 +6,38 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
-
-
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<Offset> _logoController;// should be logoAnimatin
-  late Animation<Offset> _textController;
+  late Animation<Offset> _logoAnimatin;
+  late Animation<Offset> _textAnimation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _logoAnimatin = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _textAnimation = Tween<Offset>(
+      begin: const Offset(-1, 0),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
   }
-
 
   @override
   Widget build(BuildContext context) {
