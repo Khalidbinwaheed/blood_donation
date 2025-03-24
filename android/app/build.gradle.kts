7b9293ac970cfdb541bfa4093df7f697179b2d3a
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-25 00:07:40 +0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.blood_donation"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
