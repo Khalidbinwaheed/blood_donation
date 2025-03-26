088d007efb08583410d0320a638f3628af1e015b
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-26 07:06:17 +0500

    Update size_config.dart

--- lib/util/size_config.dart
+++ lib/util/size_config.dart
@@ -6,6 +6,19 @@ class SizeConfig {
 
   static void init (BuildContext context )
   {
-    
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+  }
+
+  static double getProportionateHeight (double inputHeight)
+  {
+    double screenHeight = SizeConfig.screenHeight;
+    return (inputHeight / 812.0) * screenHeight;
+  }
+
+  static double getProportionateWidth (double inputWidth)
+  {
+    double screenWidth = SizeConfig.screenWidth;
+    return (inputWidth / 375.0) * screenWidth;
   }
 }
