e22411ee18d42991d488902b21fb42f5aae6944c
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-26 22:49:29 +0500

    Update sign_in_screen.dart

--- lib/feathers/authentication/presentation/screens/sign_in_screen.dart
+++ lib/feathers/authentication/presentation/screens/sign_in_screen.dart
@@ -1,8 +1,11 @@
+import 'package:blood_donation/common_widgets/common_button.dart';
 import 'package:blood_donation/common_widgets/common_text_field.dart';
+import 'package:blood_donation/routes/routes.dart';
 import 'package:blood_donation/util/appstyles.dart';
 import 'package:blood_donation/util/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class SignInScreen extends ConsumerStatefulWidget {
   const SignInScreen({super.key});
@@ -12,7 +15,6 @@ class SignInScreen extends ConsumerStatefulWidget {
 }
 
 class _SignInScreenState extends ConsumerState<SignInScreen> {
-
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
@@ -23,36 +25,116 @@ class _SignInScreenState extends ConsumerState<SignInScreen> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(
-          'assets/logo.png',
-          height: SizeConfig.getProportionateHeight(100),
-          width: SizeConfig.getProportionateWidth(100),
-          fit: BoxFit.cover,
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(
+          SizeConfig.getProportionateWidth(10),
+          SizeConfig.getProportionateHeight(50),
+          SizeConfig.getProportionateWidth(10),
+          0,
         ),
-        Text(
-          'Sign in to your account',
-          style: AppStyle.titleTextStyle.copyWith(color: Colors.black),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  height: SizeConfig.getProportionateHeight(100),
+                  width: SizeConfig.getProportionateWidth(100),
+                  fit: BoxFit.cover,
+                ),
+                Text(
+                  'Sign in to your account',
+                  style: AppStyle.titleTextStyle.copyWith(color: Colors.black),
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(25)),
+                CommonTextField(
+                  hintText: 'Email',
+                  textInputType: TextInputType.emailAddress,
+                  controller: _emailController,
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                CommonTextField(
+                  hintText: 'Enter Password',
+                  textInputType: TextInputType.text,
+                  controller: _passwordController,
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                CommonButton(onTap: () {}, title: 'Sign In', isLoading: false),
+                SizedBox(height: SizeConfig.getProportionateHeight(15)),
+                Text(
+                  'Or',
+                  style: AppStyle.titleTextStyle.copyWith(color: Colors.black),
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(15)),
+                GestureDetector(
+                  onTap: () {
+                    context.goNamed(
+                      AppRoutes.register.name,
+                      extra: 'Recipient',
+                    );
+                  },
+
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    width: SizeConfig.screenWidth,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2.0,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    child: Text(
+                      'Register as a Recipient',
+                      style: AppStyle.normalTextStyle.copyWith(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                GestureDetector(
+                  onTap: () {
+                    context.goNamed(AppRoutes.register.name, extra: 'Donor');
+                  },
+
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    width: SizeConfig.screenWidth,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2.0,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    child: Text(
+                      'Register as a Donor',
+                      style: AppStyle.normalTextStyle.copyWith(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(10)),
+              ],
+            ),
+          ),
         ),
-        SizedBox(height: SizeConfig.getProportionateHeight(25)),
-        CommonTextField(
-          hintText: 'Email',
-          textInputType: TextInputType.emailAddress,
-          controller: _emailController,
-        ),
-        SizedBox(height: SizeConfig.getProportionateHeight(10)),
-        CommonTextField(
-          hintText: 'Enter Password',
-          textInputType: TextInputType.text,
-          controller: _passwordController,
-        ),
-        SizedBox(height: SizeConfig.getProportionateHeight(10)),
-        
-      ],
+      ),
     );
   }
 }
