14089c9568ce6f183cbe18e12d68577b276552be
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-26 07:37:15 +0500

    Update common_text_field.dart

--- lib/common_widgets/common_text_field.dart
+++ lib/common_widgets/common_text_field.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/widgets.dart';
 
 class CommonTextField extends StatelessWidget {
-  const CommonTextField({super.key});
+  const CommonTextField({
+    super.key,
+    required this.hintText,
+    required this.textInputType,
+    this.obscureText,
+    this.controller,
+  });
+  final String hintText;
+  final TextInputType textInputType;
+  final bool? obscureText;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
