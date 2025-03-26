8b4aa9d939b8c8a4dc9f547004ec3d8d84e91f67
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-26 07:34:32 +0500

    Create common_text_field.dart

---
+++ lib/common_widgets/common_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class CommonTextField extends StatelessWidget {
+  const CommonTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
