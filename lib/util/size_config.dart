faf276d2c6c9336a6d2125cc840ff6479417a1ac
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-26 07:03:22 +0500

    Create size_config.dart

---
+++ lib/util/size_config.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static double screenHeight = 0.0;
+  static double screenWidth = 0.0;
+
+  static void init (BuildContext context )
+  {
+    
+  }
+}
