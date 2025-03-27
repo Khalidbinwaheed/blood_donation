856f0035653862e3b94a90643d6e0eb4d0f057ac
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-27 23:42:59 +0500

    Update routes.dart

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:blood_donation/feathers/user_managment/presentation/screens/main_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -21,10 +22,22 @@ final firebaseAuthProvider = Provider((ref) => FirebaseAuth.instance);
 
 @riverpod
 GoRouter goRouter(GoRouterRef ref) {
+  final firebaseAuth = ref.watch(firebaseAuthProvider);
   return GoRouter(
     initialLocation: '/splash',
     debugLogDiagnostics: true,
-    routes: [
+    redirect: (ctx, state) {
+      final isLoggedIn = firebaseAuth.currentUser != null;
+      if(isLoggedIn && state.uri.toString() == '/sign in' || state.uri.toString() == '/register') {
+        return '/main';
+      } else if (!isLoggedIn && state.uri.toString().startsWith('/main')) {
+        return '/sign in';
+      }
+      return null;
+        
+    },
+    refreshListenable: ,
+      routes: [
       GoRoute(
         name: 'splash',
         path: '/splash',
