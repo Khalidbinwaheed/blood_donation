d93e1aca87c18904731743f8fad3dead81ef8567
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-28 00:11:47 +0500

    Update sign_in_screen.dart

--- lib/feathers/authentication/presentation/screens/sign_in_screen.dart
+++ lib/feathers/authentication/presentation/screens/sign_in_screen.dart
@@ -27,110 +27,113 @@ class _SignInScreenState extends ConsumerState<SignInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppStyle.mainColor,
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(
-          SizeConfig.getProportionateWidth(10),
-          SizeConfig.getProportionateHeight(50),
-          SizeConfig.getProportionateWidth(10),
-          0,
-        ),
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/logo.png',
-                  height: SizeConfig.getProportionateHeight(100),
-                  width: SizeConfig.getProportionateWidth(100),
-                  fit: BoxFit.cover,
-                ),
-                Text(
-                  'Sign in to your account',
-                  style: AppStyle.titleTextStyle.copyWith(color: Colors.black),
-                ),
-                SizedBox(height: SizeConfig.getProportionateHeight(25)),
-                CommonTextField(
-                  hintText: 'Email',
-                  textInputType: TextInputType.emailAddress,
-                  controller: _emailController,
-                ),
-                SizedBox(height: SizeConfig.getProportionateHeight(10)),
-                CommonTextField(
-                  hintText: 'Enter Password',
-                  textInputType: TextInputType.text,
-                  controller: _passwordController,
-                ),
-                SizedBox(height: SizeConfig.getProportionateHeight(10)),
-                CommonButton(onTap: () {}, title: 'Sign In', isLoading: false),
-                SizedBox(height: SizeConfig.getProportionateHeight(15)),
-                Text(
-                  'Or',
-                  style: AppStyle.titleTextStyle.copyWith(color: Colors.black),
-                ),
-                SizedBox(height: SizeConfig.getProportionateHeight(15)),
-                GestureDetector(
-                  onTap: () {
-                    context.goNamed(
-                      AppRoutes.register.name,
-                      extra: 'Recipient',
-                    );
-                  },
-
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    width: SizeConfig.screenWidth,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
-                        style: BorderStyle.solid,
+    SizeConfig.init(context);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppStyle.mainColor,
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(
+            SizeConfig.getProportionateWidth(10),
+            SizeConfig.getProportionateHeight(50),
+            SizeConfig.getProportionateWidth(10),
+            0,
+          ),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/logo.png',
+                    height: SizeConfig.getProportionateHeight(100),
+                    width: SizeConfig.getProportionateWidth(100),
+                    fit: BoxFit.cover,
+                  ),
+                  Text(
+                    'Sign in to your account',
+                    style: AppStyle.titleTextStyle.copyWith(color: Colors.black),
+                  ),
+                  SizedBox(height: SizeConfig.getProportionateHeight(25)),
+                  CommonTextField(
+                    hintText: 'Email',
+                    textInputType: TextInputType.emailAddress,
+                    controller: _emailController,
+                  ),
+                  SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                  CommonTextField(
+                    hintText: 'Enter Password',
+                    textInputType: TextInputType.text,
+                    controller: _passwordController,
+                  ),
+                  SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                  CommonButton(onTap: () {}, title: 'Sign In', isLoading: false),
+                  SizedBox(height: SizeConfig.getProportionateHeight(15)),
+                  Text(
+                    'Or',
+                    style: AppStyle.titleTextStyle.copyWith(color: Colors.black),
+                  ),
+                  SizedBox(height: SizeConfig.getProportionateHeight(15)),
+                  GestureDetector(
+                    onTap: () {
+                      context.goNamed(
+                        AppRoutes.register.name,
+                        extra: 'Recipient',
+                      );
+                    },
+      
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      width: SizeConfig.screenWidth,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
                       ),
-                    ),
-                    child: Text(
-                      'Register as a Recipient',
-                      style: AppStyle.normalTextStyle.copyWith(
-                        color: Colors.black,
+                      child: Text(
+                        'Register as a Recipient',
+                        style: AppStyle.normalTextStyle.copyWith(
+                          color: Colors.black,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: SizeConfig.getProportionateHeight(10)),
-                GestureDetector(
-                  onTap: () {
-                    context.goNamed(AppRoutes.register.name, extra: 'Donor');
-                  },
-
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    width: SizeConfig.screenWidth,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
-                        style: BorderStyle.solid,
+                  SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                  GestureDetector(
+                    onTap: () {
+                      context.goNamed(AppRoutes.register.name, extra: 'Donor');
+                    },
+      
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      width: SizeConfig.screenWidth,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
                       ),
-                    ),
-                    child: Text(
-                      'Register as a Donor',
-                      style: AppStyle.normalTextStyle.copyWith(
-                        color: Colors.black,
+                      child: Text(
+                        'Register as a Donor',
+                        style: AppStyle.normalTextStyle.copyWith(
+                          color: Colors.black,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: SizeConfig.getProportionateHeight(10)),
-              ],
+                  SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                ],
+              ),
             ),
           ),
         ),
