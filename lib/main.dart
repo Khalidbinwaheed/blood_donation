6564d6b19451dd3543bd80752258a4febfe5e539
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-27 23:57:01 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:blood_donation/firebase_options.dart';
 import 'package:blood_donation/routes/routes.dart';
 import 'package:blood_donation/util/appstyles.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -13,12 +16,11 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-
       routerConfig: ref.watch(goRouterProvider),
       debugShowCheckedModeBanner: false,
       title: 'Blood Donation',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor:AppStyle.mainColor),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppStyle.mainColor),
         useMaterial3: true,
         appBarTheme: const AppBarTheme().copyWith(
           color: AppStyle.mainColor,
