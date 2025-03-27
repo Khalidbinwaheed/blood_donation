b5c9205ae68470889541197a0c82ee76c7dbccc9
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-28 00:11:57 +0500

    Update appstyles.dart

--- lib/util/appstyles.dart
+++ lib/util/appstyles.dart
@@ -1,19 +1,19 @@
 import 'package:blood_donation/util/size_config.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 class AppStyle {
-  static final headingTextStyle = GoogleFonts.lobsterTwo(
+  static final headingTextStyle = TextStyle(
     fontSize: SizeConfig.getProportionateHeight(20),
     color: Colors.white,
     fontWeight: FontWeight.bold,
   );
-  static final titleTextStyle = GoogleFonts.lobsterTwo(
+  static final titleTextStyle = TextStyle(
     fontSize: SizeConfig.getProportionateHeight(18),
     color: Colors.white,
     fontWeight: FontWeight.w600,
   );
-  static final normalTextStyle = GoogleFonts.lobsterTwo(
+  static final normalTextStyle = TextStyle(
     fontSize: SizeConfig.getProportionateHeight(15),
     color: Colors.white,
     fontWeight: FontWeight.w400,
