a3afb0006e15d992afaeb353c6e9860aa94a87c3
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-04-07 23:54:09 +0500

    Create auth_controller.dart

---
+++ lib/feathers/user_managment/presentation/controllers/auth_controller.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthController extends AsyncNotifier<void> {
+  @override
+  FutureOr<void> build() {
+    // Initialize any necessary resources or state here
+  }
+}
