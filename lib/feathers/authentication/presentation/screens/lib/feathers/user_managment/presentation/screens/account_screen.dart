7f417bb18d6e71bfe09fd2941b69d2f578a87b33
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-04-12 23:49:08 +0500

    Update account_screen.dart

--- lib/feathers/authentication/presentation/screens/lib/feathers/user_managment/presentation/screens/account_screen.dart
+++ lib/feathers/authentication/presentation/screens/lib/feathers/user_managment/presentation/screens/account_screen.dart
@@ -1,14 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 class AccountScreen extends ConsumerWidget { 
   const AccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Account'),
+        title: const Text('My profile information'),
       ),
       body: Center(
-        child: Text('Account Screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Account Screen'),
+            ElevatedButton(
+              onPressed: () {
+                // Add your logout logic here
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
       ),
     );
   }
