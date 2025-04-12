82cd28f242efce0c0e70529c3db0e1c27ba5cfb1
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-04-13 00:02:02 +0500

    Update account_screen.dart

--- lib/feathers/authentication/presentation/screens/lib/feathers/user_managment/presentation/screens/account_screen.dart
+++ lib/feathers/authentication/presentation/screens/lib/feathers/user_managment/presentation/screens/account_screen.dart
@@ -1,21 +1,37 @@
 import 'package:blood_donation/util/appstyles.dart';
+import 'package:blood_donation/util/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreen extends ConsumerWidget { 
+class AccountScreen extends ConsumerWidget {
   const AccountScreen({super.key});
 
   @override
-  Widget build (BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My profile information' , style: AppStyle.headingTextStyle,),
+        title: Text(
+          'My profile information',
+          style: AppStyle.headingTextStyle,
+        ),
       ),
       body: Column(
         children: [
-          Text( 'Type : User Type', style: AppStyle.titleTextStyle,)
+          Text(
+            'Type : User Type',
+            style: AppStyle.titleTextStyle,
+          ),
+          SizedBox(
+            height: SizeConfig.getProportionateHeight(20),
+          ),
+          Image.asset(
+            'assets/logo.png',
+            height: SizeConfig.getProportionateHeight(100),
+            width: SizeConfig.getProportionateWidth(100),
+            fit: BoxFit.cover,
+          ),
         ],
-      )
+      ),
     );
   }
 }
