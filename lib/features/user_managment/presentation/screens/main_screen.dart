68880e1a7aa911402d3f73568ddb7df517ef30f0
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-05-02 21:14:51 +0500

    Create main_screen.dart

---
+++ lib/features/user_managment/presentation/screens/main_screen.dart
@@ -0,0 +1,17 @@
+import 'package:blood_donation/features/user_managment/presentation/widgets/main_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MainScreen extends ConsumerWidget{
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blood Donation'),
+      ),
+      drawer: const MainDrawer(),
+    );
+  }
+}
