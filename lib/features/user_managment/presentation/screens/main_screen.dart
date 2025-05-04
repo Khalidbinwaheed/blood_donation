cbabd2171ecaa5eb544baac0c242e74afa526920
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-05-04 23:34:48 +0500

    Update main_screen.dart

--- lib/features/user_managment/presentation/screens/main_screen.dart
+++ lib/features/user_managment/presentation/screens/main_screen.dart
@@ -1,12 +1,16 @@
+import 'package:blood_donation/features/user_managment/data/firestore_repository.dart';
 import 'package:blood_donation/features/user_managment/presentation/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MainScreen extends ConsumerWidget{
+class MainScreen extends ConsumerWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final donorsAsyncValue = ref.watch(loadDonorsProvider);
+
+    ref.listen<AsyncValue>(loadDonorsProvider, (_,state){});
     return Scaffold(
       appBar: AppBar(
         title: const Text('Blood Donation'),
