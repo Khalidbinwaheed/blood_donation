1177aeb4b2f4af43ce8c432fadbd68a339348517
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-05-05 00:00:44 +0500

    Update user_item.dart

--- lib/features/user_managment/presentation/widgets/user_item.dart
+++ lib/features/user_managment/presentation/widgets/user_item.dart
@@ -1,9 +1,10 @@
+import 'package:blood_donation/features/user_managment/Domain/app_user.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserItem extends ConsumerWidget {
-  const UserItem({super.key});
-
+  const UserItem(this.appUser, {super.key});
+  final AppUser appUser ;
   @override
   Widget build (BuildContext context , WidgetRef ref){
     return 
