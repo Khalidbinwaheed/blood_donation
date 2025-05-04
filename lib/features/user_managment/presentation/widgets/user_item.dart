5b34c4351781cd176c96be21b7d7fc4f0bc2976e
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-05-04 23:59:43 +0500

    Update user_item.dart

---
+++ lib/features/user_managment/presentation/widgets/user_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserItem extends ConsumerWidget {
+  const UserItem({super.key});
+
+  @override
+  Widget build (BuildContext context , WidgetRef ref){
+    return 
+  }
+}
