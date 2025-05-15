7b661b13355dab5b0020316e14fd1e8bc060ca79
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-05-15 12:47:01 +0500

    Update blood_group_selected_screen.dart

--- lib/features/user_managment/presentation/screens/blood_group_selected_screen.dart
+++ lib/features/user_managment/presentation/screens/blood_group_selected_screen.dart
@@ -1,4 +1,7 @@
+import 'package:blood_donation/common_widgets/async_value_ui.dart';
+import 'package:blood_donation/features/user_managment/data/firestore_repository.dart';
 import 'package:blood_donation/util/appstyles.dart';
+import 'package:blood_donation/util/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,6 +12,15 @@ class BloodGroupSelectedScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    SizeConfig.init(context);
+    final donarsAsyncValue =
+        ref.watch(loadSpecificBloodGroupDonorsProvider(bloodGroup));
+    final userDataAsync =
+        ref.watch(loadSpecificBloodGroupDonorsProvider(bloodGroup));
+    ref.listen<AsyncValue>(loadSpecificBloodGroupDonorsProvider(bloodGroup),
+        (_, state) {
+      state.showAlertDialogOnError(context);
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text(
