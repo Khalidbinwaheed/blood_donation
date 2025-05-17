3d4bbf822857ac217c9b875dde007686136163eb
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-05-18 01:19:15 +0500

    Update mail_repository.dart

--- lib/features/user_managment/data/mail_repository.dart
+++ lib/features/user_managment/data/mail_repository.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
