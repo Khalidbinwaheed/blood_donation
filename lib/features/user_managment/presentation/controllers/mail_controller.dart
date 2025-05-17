3f4f0ae3f642f0ab82fdb1f6442b904e8057b0f6
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-05-17 23:25:27 +0500

    Update mail_controller.dart

---
+++ lib/features/user_managment/presentation/controllers/mail_controller.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:ffi';
+
+import 'package:blood_donation/features/user_managment/data/mail_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MailController extends AsyncNotifier<void> {
+  @override
+  FutureOr<void> build() {
+    // Initialization logic if needed
+  }
+
+  Future<bool> sendEmail(
+      {required String donorEmail,
+      required String recipientEmail,
+      required String recipientName,
+      required String recipientPhone,
+      required String recipientBloodGroup}) async {
+    try {
+      state = const AsyncLoading();
+      await ref.read(mailRepositoryProvider).sendEmail(
+          donorEmail: donorEmail,
+          recipientEmail: recipientEmail,
+          recipientName: recipientName,
+          recipientPhone: recipientPhone,
+          recipientBloodGroup: recipientBloodGroup);
+      state = const AsyncData(null);
+      return true;
+    } catch (e, StackTrace) {
+      state = AsyncError(e, StackTrace);
+      return false;
+    }
+  }
+}
