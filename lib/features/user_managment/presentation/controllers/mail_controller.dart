a638b622fcb50a34a497c13a387328afb2f3a4c1
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-06-28 22:59:44 +0500

    Update mail_controller.dart

--- lib/features/user_managment/presentation/controllers/mail_controller.dart
+++ lib/features/user_managment/presentation/controllers/mail_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:blood_donation/features/user_managment/data/mail_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'mail_controller.g.dart';
